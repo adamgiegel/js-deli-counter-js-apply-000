88498a72de95ae1fcddd1d3d77048a2ec04182b1
Adam Giegel <adam.giegel@gmail.com>
2018-09-19 01:01:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nowServing(currLine){
     if(!currLine.length){
     return "There is nobody waiting to be served!"}
     else {
-      return currLine.slice(currLine[i])
+      return currLine.slice()
   }
 }
