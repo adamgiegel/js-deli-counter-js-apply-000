64495cb01c983caa9dc8fa9280bcea7080d1fe48
Adam Giegel <adam.giegel@gmail.com>
2018-09-19 01:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,5 @@ function currentLine(currLine){
   for(let i = 0; i < currLine.length; i++){
       newStr += i+1 + '. ' + currLine[i] + ', '
     }
-    newStr.slice(0, -1)
-    return "The line is currently: " + newStr
+    return "The line is currently: " + newStr.slice(0, -1)
 }
