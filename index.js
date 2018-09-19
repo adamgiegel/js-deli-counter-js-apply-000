5e1926f74aa1e856efd94998845b52c5f31015b9
Adam Giegel <adam.giegel@gmail.com>
2018-09-19 00:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(currLine, newName){
   if(!currLine.length){currLine.push(newName)
-  return `Welcome ${newName}. You are number 1 in line.`}
+  return `Welcome, ${newName}. You are number 1 in line.`}
   if(currLine.length > 0){currLine.push(newName)
   return `Welcome, ${newName}. You are number ${currLine.length} in line.`}
 }
